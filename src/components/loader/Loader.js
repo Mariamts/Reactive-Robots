49da88b51b03141cb8fe4618ba3533a36d4b476d
ChanksM <mushni.chankseliani@geolab.edu.ge>
2021-06-17 21:39:39 +0400

    Feat(): Create base structure

---
+++ src/components/loader/Loader.js
@@ -0,0 +1,5 @@
+export const Loader = ({ message }) => (
+  <div className="row">
+    <h2 className="text-center text-warning">{message}</h2>
+  </div>
+);
---
+++ src/pages/profile/profile.js
@@ -0,0 +1,32 @@
+import classNames from 'classnames';
+import { useDispatch } from 'react-redux';
+import { useHistory } from 'react-router';
+import { withAuthProtected } from '../../hoc';
+import { setAuthGuestAction } from '../../redux/actions';
+import { logOut } from '../../services';
+
+import css from './profile.module.css';
+// eslint-disable-next-line
+function Profile({ title = '' }) {
+  const history = useHistory();
+  const dispatch = useDispatch();
+
+  const onLogOut = async () => {
+    await logOut();
+
+    dispatch(setAuthGuestAction());
+
+    history.replace('/');
+  };
+
+  return (
+    <div className={classNames('row mt-3 p-3', css.profile)}>
+      <button type="button" className="btn btn-light btn-lg mb-3" onClick={onLogOut}>
+        Log Out
+      </button>
+      <h2 className={classNames(css.title)}>{title}</h2>
+    </div>
+  );
+}
+
+export default withAuthProtected(Profile);
