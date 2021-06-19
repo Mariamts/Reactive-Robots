b3278ba10f64291e839a44d2facdf41a8316e0c2
ChanksM <mushni.chankseliani@geolab.edu.ge>
2021-06-19 14:00:12 +0400

    Feat(): update header

--- src/components/navigation/navigation.js
+++ src/components/navigation/navigation.js
@@ -10,61 +10,63 @@ function Navigation() {
   const authorized = useSelector(authSelector);
 
   return (
-    <div className="row mt-5 mb-3">
-      <ul className="nav nav-pills">
-        <li className="nav-item">
-          <NavLink
-            className="nav-link"
-            to={routes.HOME_PATH}
-            exact
-            activeClassName={css['active-class']}>
-            Home Page
-          </NavLink>
-        </li>
+    <nav className="navbar navbar-expand-lg navbar-light bg-light mb-4 p-3">
+      <div className="container-fluid">
+        <a className="navbar-brand"></a>
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav">
+            <li className="nav-item">
+              <NavLink
+                className="nav-link"
+                to={routes.HOME_PATH}
+                exact
+                activeClassName={css['active-class']}>
+                Home Page
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink
+                className="nav-link"
+                to={routes.FAKER_ROBOTS}
+                exact
+                activeClassName={css['active-class']}>
+                Faker Robots
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink
+                className="nav-link"
+                to={routes.CUSTOM_ROBOTS}
+                exact
+                activeClassName={css['active-class']}>
+                Custom Robots
+              </NavLink>
+            </li>
+            {authorized && (
+              <li className="nav-item">
+                <NavLink
+                  className="nav-link"
+                  to={routes.PROFILE_PATH}
+                  activeClassName={css['active-class']}>
+                  Profile
+                </NavLink>
+              </li>
+            )}
 
-        <li className="nav-item">
-          <NavLink
-            className="nav-link"
-            to={routes.FAKER_ROBOTS}
-            exact
-            activeClassName={css['active-class']}>
-            Faker Robots
-          </NavLink>
-        </li>
-
-        <li className="nav-item">
-          <NavLink
-            className="nav-link"
-            to={routes.CUSTOM_ROBOTS}
-            exact
-            activeClassName={css['active-class']}>
-            Custom Robots
-          </NavLink>
-        </li>
-
-        {authorized && (
-          <li className="nav-item">
-            <NavLink
-              className="nav-link"
-              to={routes.PROFILE_PATH}
-              activeClassName={css['active-class']}>
-              Profile
-            </NavLink>
-          </li>
-        )}
-
-        {!authorized && (
-          <li className="nav-item">
-            <NavLink
-              className="nav-link"
-              to={routes.AUTH_PATH}
-              activeClassName={css['active-class']}>
-              AuthPage
-            </NavLink>
-          </li>
-        )}
-      </ul>
-    </div>
+            {!authorized && (
+              <li className="nav-item">
+                <NavLink
+                  className="nav-link"
+                  to={routes.AUTH_PATH}
+                  activeClassName={css['active-class']}>
+                  AuthPage
+                </NavLink>
+              </li>
+            )}
+          </ul>
+        </div>
+      </div>
+    </nav>
   );
 }
 
